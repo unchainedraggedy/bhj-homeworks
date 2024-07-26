bdd43f7077a1f7af1097763a571728cc252c3468
Ash <redcoolike@gmail.com>
2024-07-26 17:10:26 +0300

    mod task

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,7 +1,31 @@
 const timer = document.getElementById('timer');
 
+/*
 const changeTimer = () => {
     timer.textContent -= 1;
 }
 
 setInterval(changeTimer, 1000)
+*/
+
+let seconds = +timer.textContent
+
+const changeTimer = () => {
+    if (seconds > 0) {
+        seconds -= 1;
+    }
+
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor((seconds % 3600) / 60);
+    let leftSec = seconds % 60;
+
+    let hoursToHours = (hours < 10 ? '0' : '') + hours;
+    let minutesToMinutes = (minutes < 10 ? '0' : '') + minutes;
+    let secToSec = (leftSec < 10 ? '0' : '') + leftSec;
+
+    const formattedTime = hoursToHours + ':' + minutesToMinutes + ':' + secToSec;
+
+    timer.textContent = formattedTime;
+}
+
+setInterval(changeTimer, 1000);
