0b82e87e1fbe33e4e7579c11d2b696bfd55bd752
Ash <redcoolike@gmail.com>
2024-07-29 18:09:04 +0300

    mole task

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,33 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+
+const getHole = index => document.getElementById(`hole${index}`);
+
+const clicker = mole => {
+    if(mole.target.classList.contains('hole_has-mole')){
+      dead.textContent = +dead.textContent + 1
+    } else {
+        lost.textContent = +lost.textContent + 1
+    }
+
+    if(+dead.textContent === 9){
+        dead.textContent = 0;
+        lost.textContent = 0;
+        alert('Вы выиграли!')
+    }
+    if(+lost.textContent === 5){
+        dead.textContent = 0;
+        lost.textContent = 0;
+        alert('Бадум-тс)))): Попробуйте ещё')
+    }
+}
+
+for(let i = 1; i < 9; i++){
+    getHole(i).addEventListener('click', clicker);
+}
+
+
+
+
+
+
