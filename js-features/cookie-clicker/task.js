adeaf36e28d49ed5a2a2afb541840511957ddf68
Ash <redcoolike@gmail.com>
2024-07-29 19:25:22 +0300

    interval

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cookie = document.getElementById('cookie');
     const clickerCounter = document.getElementById('clicker__counter');
+    const clicksPerSecond = document.getElementById('clicks-per-second_span');
     let counter = +clickerCounter.textContent
+    let intervalId;
+   
+
+    let timeThen = Date.now()
+    let clicksSecondsCounter = 0;
 
     cookie.addEventListener('click', () => {
         counter++;
         clickerCounter.textContent = counter;
+        const timeNow = Date.now();
+
+        if((timeNow - timeThen) > 0){
+            clicksSecondsCounter = 1 / ((timeNow - timeThen) / 1000);
+
+        }
+        clicksPerSecond.textContent = Math.floor(clicksSecondsCounter);
 
         if(cookie.style.width === '200px'){
             cookie.style.width = '100px';
@@ -13,5 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             cookie.style.width = '200px';
         }
+
+        timeThen = timeNow;
     })
+    intervalId = setInterval(() => {
+        clicksPerSecond()
+    }, 1000)
 })
+
