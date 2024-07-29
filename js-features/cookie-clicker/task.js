cf7cf6c8802d942fc05b95435d72526a5d422d4d
Ash <redcoolike@gmail.com>
2024-07-29 17:20:40 +0300

    shortext cookie decision

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const clickerCounter = document.getElementById('clicker__counter');
     const cookie = document.getElementById('cookie');
-    let counter = 0;
+    const clickerCounter = document.getElementById('clicker__counter');
+    let counter = +clickerCounter.textContent
 
     cookie.addEventListener('click', () => {
         counter++;
         clickerCounter.textContent = counter;
 
-        if (cookie.style.width === '200px' || cookie.style.width === '') {
+        if(cookie.style.width === '200px'){
             cookie.style.width = '100px';
             cookie.style.height = '100px';
         } else {
             cookie.style.width = '200px';
-            cookie.style.height = '200px';
         }
-    });
-});
+    })
+})
