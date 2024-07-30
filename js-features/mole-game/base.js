9fa5c352b5f8e30df64d2d3df926810916db101c
Ash <redcoolike@gmail.com>
2024-07-30 06:39:13 +0300

    excess

--- js-features/mole-game/base.js
+++ js-features/mole-game/base.js
@@ -1,17 +1,12 @@
 (() => {
-  let playing = true,
     activeHole = 1;
 
-  const stop = () => playing = true,
     getHole = index => document.getElementById(`hole${index}`),
     deactivateHole = index =>
       getHole( index ).className = 'hole',
     activateHole = index =>
       getHole( index ).className = 'hole hole_has-mole',
     next = () => setTimeout(() => {
-      if ( !playing ) {
-        return;
-      }
       deactivateHole( activeHole );
       activeHole = Math.floor( 1 + Math.random() * 9 );
       activateHole( activeHole );
@@ -20,3 +15,30 @@
 
   next();
 })();
+
+/*
+function startGame(){
+  let activateHole = 1;
+  
+  function getHole(index){
+    document.getElementById(`hole${index}`)
+  }
+  function deactivateHole(index){
+    getHole(index).className = 'hole';
+  }
+  function activateHole(index){
+    getHole(index).className = 'hole hole_has-mole';
+  }
+  function next(){
+    setTimeout(() => {
+      deactivateHole(activeHole);
+      activeHole = Math.floor(1 + Math.random() * 9);
+      activeHole(activeHole);
+      next();
+    }, 800);
+  }
+  next();
+}
+
+startGame()
+*/
