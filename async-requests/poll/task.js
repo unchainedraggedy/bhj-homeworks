2d1e43186016067e91a4b221f9fe31b2f0c7c709
Ash <redcoolike@gmail.com>
2024-09-18 14:55:32 +0300

    tasked.js

---
+++ async-requests/poll/task.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pollTitle = document.getElementById('poll__title');
+    const pollAnswers = document.getElementById('poll__answers');
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      const poll = xhr.response;
+      pollTitle.textContent = poll.data.title;
+  
+      poll.data.answers.forEach((answer, index) => {
+        const button = document.createElement('button');
+        button.className = 'poll__answer';
+        button.textContent = answer;
+        button.addEventListener('click', () => {
+          alert('Спасибо, ваш голос засчитан!');
+          const xhrPost = new XMLHttpRequest();
+          xhrPost.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
+          xhrPost.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+          xhrPost.onload = () => {
+            const result = JSON.parse(xhrPost.response);
+            pollAnswers.innerHTML = '';
+            result.stat.forEach(stat => {
+              const statEl = document.createElement('div');
+              statEl.textContent = `${stat.answer}: ${stat.votes} голосов`;
+              pollAnswers.appendChild(statEl);
+            });
+          };
+          xhrPost.send(`vote=${poll.id}&answer=${index}`);
+        });
+        pollAnswers.appendChild(button);
+      });
+    };
+    xhr.send();
+  });
+  
