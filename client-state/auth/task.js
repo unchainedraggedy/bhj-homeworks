db9654632cf4b4e3c66fcf21755fe50c82f52347
Ash <redcoolike@gmail.com>
2024-09-27 21:08:25 +0300

    client-state

---
+++ client-state/auth/task.js
@@ -0,0 +1,44 @@
+const signin = document.getElementById('signin');
+const form = document.getElementById('signin__form');
+const loginForm = form.login;
+const passwordForm = form.password;
+
+const welcome = document.getElementById('welcome');
+const userIdSpan = document.getElementById('user_id');
+
+const logout = document.getElementById('logout');
+const logoutBtn = document.getElementById('logout__btn')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const xhrPost = new XMLHttpRequest();
+
+    xhrPost.open('POST', form.action);
+    xhrPost.send(formData);
+
+    xhrPost.onload = () => {
+        const response = JSON.parse(xhrPost.response);
+
+        if(response.success){
+            localStorage.setItem('user_id', response.user_id);
+            signin.classList.remove('signin_active');
+            welcome.classList.add('welcome_active');
+            userIdSpan.innerText = response.user_id;
+        } else {
+            alert('Неверный логин и/или пароль')
+        }
+    }
+    loginForm.value = '';
+    passwordForm.value = '';
+})
+
+logoutBtn.addEventListener('click', () => {
+    signin.classList.add('signin_active');
+    welcome.classList.remove('welcome_active');
+    loginForm.value = '';
+    passwordForm.value = '';
+})
+  
+  
