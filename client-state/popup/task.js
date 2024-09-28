de2f0e58739a5341900bfe1c58ad6576b8c1b6ef
Ash <redcoolike@gmail.com>
2024-09-28 20:33:55 +0300

    popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,32 +1,5 @@
-// function setCookie(name, value){
-//     document.cookie = name + '=' + encodeURIComponent(value);
-// }
-
-// const modal = document.getElementById('subscribe-modal');
-// const modalClose = document.getElementsByClassName('modal__close')[0];
-
-// modalClose.addEventListener('click', () => {
-//     modal.classList.remove('modal_active');
-//     setCookie('modalClosed', 'true')
-// });
-
-// function getCookie(name){
-//     const cookies = document.cookie.split('; ');
-//     for(let cookie of cookies){
-//         const [cookieName, cookieValue] = cookie.split('=');
-//         if(cookieName === name){
-//             return cookieValue
-//         }
-//     }
-// }
-
-// if(!getCookie('modalClosed')){
-//     modal.classList.add('modal_active')
-// }
-
-
 function setCookie(name, value) {
-    document.cookie = name + '=' + encodeURIComponent(value);
+    document.cookie = name + '=' + encodeURIComponent(value) + '; path=/';
 }
 
 function getCookie(name) {
@@ -37,9 +10,8 @@ function getCookie(name) {
             return decodeURIComponent(cookieValue);
         }
     }
-    return undefined;
-
 }
+
 const modal = document.getElementById('subscribe-modal');
 const closeModal = document.getElementsByClassName('modal__close')[0];
 
