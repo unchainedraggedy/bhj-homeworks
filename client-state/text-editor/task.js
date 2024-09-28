64b45ef4adb4d00e8bba68f07a7104687d834b47
Ash <redcoolike@gmail.com>
2024-09-28 17:04:39 +0300

    btn to text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,4 +1,5 @@
 const editor = document.getElementById('editor');
+const card = document.getElementsByClassName('card')[0];
 
 editor.addEventListener('input', () => {
     localStorage.setItem('savedText', editor.value);
@@ -8,3 +9,12 @@ const savedText = localStorage.getItem('savedText');
 if(savedText){
     editor.value = savedText;
 }
+
+const btn = document.createElement('button');
+btn.style.display = 'block';
+btn.innerText = 'очистить';
+card.appendChild(btn);
+
+btn.addEventListener('click', () =>{
+    editor.value = '';
+})
